6ccaf18793d4a627d5f8546b6e0ec43b2c5d2488
szbgreg <74787170+szbgreg@users.noreply.github.com>
2025-12-04 23:13:36 +0100

    feat: add seperate enemy and player board

--- app.py
+++ app.py
@@ -11,24 +11,46 @@ def create_board():
     
     return board
 
-""" Tábla kirajzolása """
-def print_board(board):
+""" Játékosok tábláinak kirajzolása """
+def print_boards(board):
+    print()
+    print("*" * 55)
+    print(" " * 10 + "ENEMY" + " " * 24 + "PLAYER")
+    print()
 
-    # Oszlop jelölések
-    print("   ", end="")
+    # Ellenfél tábla oszlopjelölései
+    print(" " * 3, end="")
     for i in range(10):
         print(chr(65 + i), end=" ")
+        
+    print(" " * 10, end="")
+
+    # Saját tábla oszlopjelölései
+    for i in range(10):
+        print(chr(65 + i), end=" ")
+
     print()
 
     # Sorok kirajzolása
     for i in range(10):
-        # Sorszámok
+        # Ellenfél tábla sorjelölései
         print(f"{i+1:2d} ", end="")
 
+        # Ellenfél tábla sorai
+        for j in range(10):
+            print(board[i][j], end=" ")
+
+        print(" " * 7, end="")
+
+        # Saját tábla sorjelölései
+        print(f"{i+1:2d} ", end="")
+
+        # Saját tábla sorai
         for j in range(10):
             print(board[i][j], end=" ")
         print()
 
         
-board = create_board()
-print_board(board)
+player_board = create_board()
+enemey_board = create_board()
+print_boards(player_board)
